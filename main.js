38508aecfe5859a69036706b5e555b2bf5d5796a
Rishabh Shukla <73129563+rishabhshukla489@users.noreply.github.com>
2022-09-22 18:11:53 +0530

    Update main.js
    Implemented the function to give required result.

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 const assert = require('assert');
 
 function countBatteriesByUsage(cycles) {
+  var lowCount=0;
+  var mediumCount=0;
+  var highCount=0;
+  cycles.forEach(cycle=> {
+    if(cycle<410){
+      lowCount+=1;
+    }else if(cycle>=410 && cycle<909){
+      mediumCount+=1;
+    }else{
+      highCount+=1;
+    }
+  });
+
+
   return {
-    lowCount: 0,
-    mediumCount: 0,
-    highCount: 0
+    lowCount,
+    mediumCount,
+    highCount
   };
 }
 
 function testBucketingByNumberOfCycles() {
   console.log('Counting batteries by usage cycles...')
-  counts = countBatteriesByUsage([100, 300, 500, 600, 900, 1000]);
+  counts = countBatteriesByUsage([100, 300, 410, 500, 600, 900, 909, 1000]); //test with boundary conditions
   assert(counts["lowCount"] == 2);
-  assert(counts["mediumCount"] == 3);
-  assert(counts["highCount"] == 1);
+  assert(counts["mediumCount"] == 4);
+  assert(counts["highCount"] == 2);
   console.log("Done counting :)");
 }
 
